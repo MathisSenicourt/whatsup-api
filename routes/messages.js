b3aa9c6267ddef90ecf1b5aaa72b7b4013ba900e
MathisSenicourt <mathiscience@hotmail.fr>
2024-05-30 10:11:18 +0200

    fix merge

--- routes/messages.js
+++ routes/messages.js
@@ -4,16 +4,16 @@ const messageService = require('../services/messages');
 const jwt = require("jsonwebtoken");
 
 // // Route pour sauvegarder/envoyer un message
-router.post('/send-message', async function (req, res, next) {
-    const {sender, receiver, content} = req.body;
-    try {
-        const result = await messageService.sendMessage(sender, receiver, content);
-        res.json(result);
-    } catch (error) {
-        console.error('Error sending message:', error);
-        res.status(500).json({error: 'An error occurred while sending the message'});
-    }
-});
+// router.post('/send-message', async function (req, res, next) {
+//     const {sender, receiver, content} = req.body;
+//     try {
+//         const result = await messageService.sendMessage(sender, receiver, content);
+//         res.json(result);
+//     } catch (error) {
+//         console.error('Error sending message:', error);
+//         res.status(500).json({error: 'An error occurred while sending the message'});
+//     }
+// });
 
 // Route pour récupérer la liste des conversations récentes de l'utilisateur
 router.get('/recent-conversations', async function (req, res, next) {
